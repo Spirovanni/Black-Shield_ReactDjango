ca349227fc7a5246d256472a9257fd0251ed2b8a
Xavier Martinez <blackshieldsx@gmail.com>
2022-03-15 14:42:25 -0700

    Creating the serializer

--- blkshlds-api/base_api/views.py
+++ blkshlds-api/base_api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
+from .models import Card, Rating
+from .serializers import CardSerializer, RatingSerializer, UserSerializer
