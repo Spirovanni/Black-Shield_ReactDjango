68dca1203d36927f83e4ea818dadbef2f2c9d27c
Xavier Martinez <blackshieldsx@gmail.com>
2022-03-16 21:54:41 -0700

    Connecting user serializer instance

--- blkshlds-api/base_api/views.py
+++ blkshlds-api/base_api/views.py
@@ -1,11 +1,51 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
 from .models import Card, Rating
-from .serializers import CardSerializer, RatingSerializer
+from .serializers import CardSerializer, RatingSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+
+    @action(detail=True, methods=['POST'])
+    def rate_card(self, request):
+        response = {'message': 'Its working'}
+        return Response(response, status=status.HTTP_202_ACCEPTED)
+
+    @action(detail=True, methods=['POST'])
+    def rate_card(self, request, pk=None):
+        if 'stars' in request.data:
+
+            card = Card.objects.get(id=pk)
+            stars = request.data['stars']
+            user = request.user
+
+            try:
+                rating = Rating.objects.get(user=user.id, card=card.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, card=card, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+        else:
+            response = {'message': 'You need to provide stars'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
